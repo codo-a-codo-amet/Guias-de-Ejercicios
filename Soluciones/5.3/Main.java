6f701fbaa1ae303b77d54943f7d4a5f5e69be33e
Esteban Borai <leonardoestebanbba@gmail.com>
2017-10-04 14:59:09 -0300

    Create Main.java

---
+++ Soluciones/5.3/Main.java
@@ -0,0 +1,10 @@
+package com.amet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TablaDeMultiplicar tabla = new TablaDeMultiplicar(0);
+        tabla.init();
+    }
+    
+}
