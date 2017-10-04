015b8dae57272cc805e374fab9a2ea6800446a90
Esteban Borai <leonardoestebanbba@gmail.com>
2017-10-04 15:40:20 -0300

    Create Main.java

---
+++ Soluciones/Uriburu/1.1/Main.java
@@ -0,0 +1,10 @@
+package com.amet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PorcentajeDescontado pD = new PorcentajeDescontado(13.00, 15.00);
+        pD.calc();
+    }
+    
+}
